3ff2cbe2167ddb1872f09b08ffc1d51cebe4ded6
Takumichan <37031501+Taku-firi@users.noreply.github.com>
2019-11-04 16:39:15 +0800

    Update FileItem.java

--- app/src/main/java/com/Stardust/cabicat/item/FileItem.java
+++ app/src/main/java/com/Stardust/cabicat/item/FileItem.java
@@ -6,14 +6,16 @@ import java.util.Date;
 public class FileItem {
     private String name;
     private String path;
+    private Long filesize;
     private String addeddate;
     private String checkdate;
     private int priority;
     SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    public FileItem(String name, String path){
+    public FileItem(String name, String path, Long filesize){
         this.name = name;
         this.path = path;
+        this.filesize = filesize;
         this.addeddate = dateformat.format(System.currentTimeMillis());
     }
 
@@ -24,6 +26,10 @@ public class FileItem {
 
     public String getPath(){
         return path;
+    }
+
+    public Long getFilesize(){
+        return filesize;
     }
 
     public String getAddeddate(){
