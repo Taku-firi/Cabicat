e1232ab8eb5c904fd86b1eb7970f68146d83d480
sida <1017493647@qq.com>
2019-11-06 17:42:02 +0800

    test-databasehelper
    test table layout

--- app/src/main/java/com/Stardust/cabicat/database/DatabaseHelper.java
+++ app/src/main/java/com/Stardust/cabicat/database/DatabaseHelper.java
@@ -33,8 +33,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     + NAME + " TEXT,"
                     + SIZE + " LONG,"
                     + ADDEDDATE + " DATETIME,"
-                    + CHECKDATE + "DATETIME,"
-                    + PRIORITY + "INTEGER"
+                    + CHECKDATE + " DATETIME,"
+                    + PRIORITY + " INTEGER"
                     + ")";
     private static final String CREATE_TABLE_SECRETLAYER =
             "CREATE TABLE " + TABLE_SECRETLAYER
@@ -43,8 +43,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     + NAME + " TEXT,"
                     + SIZE + " LONG,"
                     + ADDEDDATE + " DATETIME,"
-                    + CHECKDATE + "DATETIME,"
-                    + PRIORITY + "INTEGER"
+                    + CHECKDATE + " DATETIME,"
+                    + PRIORITY + " INTEGER"
                     + ")";
     //
     private static DatabaseHelper sInstance;
